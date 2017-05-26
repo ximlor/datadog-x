bcbfda04146f353ca902b06a181d8995aeef1c1a
ximlor <2705833399@qq.com>
2017-05-26 17:23:38 +0800

    append laravel-ide-helper

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // 开发环境
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);  // IDE 帮助
+        }
     }
 }
